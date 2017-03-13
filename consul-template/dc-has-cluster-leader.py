380823c2549c797fdea706c766e155bc70ea02a0
Anth Courtney <anthcourtney@gmail.com>
2017-03-13 19:30:07 +1100

    Updated plugin

--- consul-template/dc-has-cluster-leader.py
+++ consul-template/dc-has-cluster-leader.py
@@ -3,17 +3,19 @@ import os
 import sys
 import requests
 
-if len(sys.argv) > 1 and "CONSUL_ADDR" in os.environ:
+consul_var = "CONSUL_ADDR"
+result = "false"
+
+if len(sys.argv) > 1 and consul_var in os.environ:
   dc = sys.argv[1]
   # query consul
-  r = requests.get( "http://" + os.environ['CONSUL_ADDR'] + "/v1/catalog/services", params={ 'dc': dc })
+  r = requests.get( "http://" + os.environ[consul_var] + "/v1/catalog/services", params={ 'dc': dc })
   if r.status_code == requests.codes.ok:
-    print "true"
-  else:
-    print "false"
+    result = "true"
 elif len(sys.argv) > 1:
   # dc provided, but no consul_addr set, so we can't verify. Therefore assume cluster leader exists
-  print "true"
-else:
-  # No argv = no dc = no cluster leader
-  print "false"
+  result = "true"
+
+# Exit
+print result
+sys.exit(0)
